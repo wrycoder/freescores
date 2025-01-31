d1d021945464223617ed1d883d19502a06ee2edc
Michael Toulouse <wrycoder@ameritech.net>
2025-01-31 11:15:42 +0000

    Adding handy reference to development URL to config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,9 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  # DEVELOPMENT URL:
+  # http://3.139.72.192:8181/
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
