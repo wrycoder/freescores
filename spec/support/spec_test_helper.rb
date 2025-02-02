3d94278b3a58572394c83e56e4c1b7ade1e630a9
Michael Toulouse <wrycoder@ameritech.net>
2025-02-02 11:34:04 +0000

    Updating login helper

--- spec/support/spec_test_helper.rb
+++ spec/support/spec_test_helper.rb
@@ -2,12 +2,10 @@ module SpecTestHelper
   # Logs the administrator in through SessionsController
   def log_in_through_controller(options = {})
     options[:password] ||= 'password'
-    options[:remember_me] ||= '1'
     options[:forwarding_url] ||= root_path
-    post login_path,
+    post "/sessions/create",
       :params => {
         :session => { :password => options[:password],
-                      :remember_me => options[:remember_me],
                       :forwarding_url => options[:forwarding_url] }
       }
   end
