bd823c203e7a32d5a2acb3eb689900099f01b349
Michael Toulouse <wrycoder@ameritech.net>
2025-02-02 11:26:02 +0000

    Adding app_title helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  # Returns a context-sensitive title
+  def app_title(page_title = '')
+    base_title = "Rick Sowash"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
+
 end
