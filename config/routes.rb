65a398e107c7b8fafd6617485eafa2ae58b4d9a7
Michael Toulouse <wrycoder@ameritech.net>
2025-02-02 11:29:46 +0000

    Define resources for works, instruments, and genres

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+  get 'dashboard/show'
+  post 'sessions/create'
+  get 'sessions/destroy'
+  get 'sessions/new'
 
-  resources :works
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :works
+  resources :instruments, only: [:new, :create, :show,
+                                 :index, :edit, :update]
+  resources :genres, only: [:new, :create, :index, :edit, :update]
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
