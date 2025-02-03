a9853af376f107d60dfac3322cbf4f4b406addd7
Michael Toulouse <wrycoder@ameritech.net>
2025-02-03 12:12:37 +0000

    Adding capability to create new genres

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -2,14 +2,28 @@ class GenresController < ApplicationController
   before_action :logged_in_user, only: [:new, :create, :edit]
 
   def new
+    @genre = Genre.new
   end
 
   def create
+    begin
+      Genre.create!(genre_params)
+    rescue ActiveRecord::RecordInvalid => ex
+      flash.now.alert = "#{ex.message}"
+    end
+    @genres = Genre.all
+    render "index"
   end
 
   def index
+    @genres = Genre.all
   end
 
   def edit
   end
+
+  private
+    def genre_params
+      params.require(:genre).permit(:name, :vocal)
+    end
 end
