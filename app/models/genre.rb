c7ba031bb650bdd1068e24267f0afeb6b7a1f5c7
Michael Toulouse <wrycoder@ameritech.net>
2025-02-03 12:13:28 +0000

    Make sure genre has a unique name

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,5 +1,5 @@
 class Genre < ApplicationRecord
   has_many :works, dependent: :destroy
   validates :vocal, inclusion: [true, false]
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
 end
