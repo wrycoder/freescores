1e34326a76d88278c4b29b133411b632f9a00267
Michael Toulouse <wrycoder@ameritech.net>
2025-02-03 12:19:40 +0000

    Removing ability to edit genres from routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   resources :works
   resources :instruments, only: [:new, :create, :show,
                                  :index, :edit, :update]
-  resources :genres, only: [:new, :create, :index, :edit, :update]
+  resources :genres, only: [:new, :create, :index, :update]
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
