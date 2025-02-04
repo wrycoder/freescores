ce4f88e028764898ee6d7e81cccb20758d3f7ee9
Michael Toulouse <wrycoder@ameritech.net>
2025-02-04 16:49:10 +0000

    Deal with weird params in a new class method

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -2,9 +2,28 @@ class WorksController < ApplicationController
   before_action :logged_in_user, only: [:new, :create, :update, :edit]
 
   def new
+    @work = Work.new
+    6.times { @work.parts.build }
   end
 
   def create
+    begin
+      # work_params["parts_attributes"] = work_params["parts_attributes"].except(empty_fields)
+      @work = Work.build_from_params(work_params)
+    rescue ActiveRecord::RecordNotFound => ex
+      flash.now[:alert] = "Invalid instrument"
+      @work = Work.new
+      render "create", :status => :bad_request and return
+    end
+    begin
+      @work.save!
+    rescue ActiveRecord::ActiveRecordError => ex
+      flash.now[:alert] = "Validation failed"
+      render "create", :status => :bad_request and return
+    end
+    flash.now[:notice] = "Work created"
+    @works = Work.all
+    render "index"
   end
 
   def index
