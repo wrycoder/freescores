3af991eacbd153fdcf99097a92bf2c89535d88b7
Michael Toulouse <wrycoder@ameritech.net>
2025-02-04 16:45:34 +0000

    Adding view specs for works/{edit,new}

---
+++ spec/views/works/edit.html.erb_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe "works/edit.html.erb", type: :view do
+  it "shows the title" do
+    genre = create(:genre)
+    piano = create(:instrument,
+                  name: "piano",
+                  rank: 550,
+                  family: "keyboard")
+    flute = create(:instrument,
+                  name: "flute",
+                  rank: 500,
+                  family: "woodwind")
+    work = build(:work, genre_id: genre.id)
+    work.add_instruments({ piano => 1, flute => 1 })
+    work.save!
+    assign(:work, work)
+    render
+    page = Nokogiri::HTML(rendered)
+    hidden_tag = page.xpath('//form/input').first
+    expect(hidden_tag.attribute_nodes[0].value).to match(/hidden/)
+    expect(hidden_tag.attribute_nodes[1].value).to match(/_method/)
+    expect(hidden_tag.attribute_nodes[2].value).to match(/patch/)
+    work.destroy
+  end
+end
---
+++ spec/views/works/new.html.erb_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe "works/new.html.erb", type: :view do
+  it "shows a valid form" do
+    genre = create(:genre)
+    piano = create(:instrument,
+                  name: "piano",
+                  rank: 550,
+                  family: "keyboard")
+    flute = create(:instrument,
+                  name: "flute",
+                  rank: 500,
+                  family: "woodwind")
+    render
+    expect(rendered).to match(/Title/)
+    expect(rendered).to match(/PDF Score/)
+    page = Nokogiri::HTML(rendered)
+    expect(page.css('.genre-selector')).to_not be nil
+    genre_selector = page.css('.genre-selector')
+    expect(genre_selector[0].children[2].children[0])
+          .to match(/#{genre.name}/)
+    part_selector = page.css('.part-selector')
+    expect(part_selector[0].children[2].children[0])
+          .to match(/piano/)
+  end
+end
