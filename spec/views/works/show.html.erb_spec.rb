2bfb5566fca90917e7e1f2422b80762c1b1b193b
Michael Toulouse <wrycoder@ameritech.net>
2025-02-06 02:37:01 +0000

    More specs for works

--- spec/views/works/show.html.erb_spec.rb
+++ spec/views/works/show.html.erb_spec.rb
@@ -1,34 +1,72 @@
 require 'rails_helper'
 
 RSpec.describe "works/show.html.erb", type: :view do
-  it "shows the title" do
-    genre = create(:genre)
-    piano = create(:instrument,
+  before :each do
+    @genre = create(:genre)
+    @piano = create(:instrument,
                   name: "piano",
                   rank: 550,
                   family: "keyboard")
-    flute = create(:instrument,
+    @flute = create(:instrument,
                   name: "flute",
                   rank: 500,
                   family: "woodwind")
-    work_1 = build(:work, genre_id: genre.id)
-    work_1.add_instruments({ piano => 1, flute => 1 })
+
+    if !ENV['FILE_HOST'].nil?
+      @original_file_host = ENV['FILE_HOST']
+    else
+      ENV['FILE_HOST'] = 'ourserver.com'
+      @original_file_host = nil
+    end
+
+    if !ENV['FILE_ROOT'].nil?
+      @original_file_root = ENV['FILE_ROOT']
+    else
+      ENV['FILE_ROOT'] = 'recordings/mp3'
+      @original_file_root = nil
+    end
+  end
+
+  after :each do
+    Work.destroy_all
+    Instrument.destroy_all
+    Genre.destroy_all
+    ENV['FILE_HOST'] = @original_file_host
+    ENV['FILE_ROOT'] = @original_file_root
+  end
+
+  it "shows the instruments and revision" do
+    work_1 = build(:work, genre_id: @genre.id)
+    work_1.add_instruments({ @piano => 1, @flute => 1 })
     work_1.save!
     assign(:work, work_1)
     render
-    expect(rendered).to match(/\(flute and piano\)/)
-    expect(rendered).to_not match(/Revised in/)
+    expect(rendered).to match(/flute and piano/)
+    expect(rendered).to_not match(/Revised/)
     work_2 = build(:work,
-      genre_id: genre.id,
+      genre_id: @genre.id,
       composed_in: 1996,
       revised_in: 2018)
-    work_2.add_instruments({ flute => 1 })
+    work_2.add_instruments({ @flute => 1 })
     work_2.save!
     assign(:work, work_2)
     render
-    expect(rendered).to match(/\(flute\)/)
-    expect(rendered).to match(/Revised in/)
+    expect(rendered).to match(/flute/)
+    expect(rendered).to match(/Revised/)
     work_1.destroy
     work_2.destroy
+  end
+
+  it "shows links to score and mp3" do
+    work = build(:work, genre_id: @genre.id)
+    work.add_instruments({ @piano => 1 })
+    work.score_link = work.title.gsub(' ', '_') + ".pdf"
+    work.recording_link = work.title.gsub(' ', '_') + ".mp3"
+    work.save!
+    assign(:work, work)
+    render
+    expect(rendered).to match(/recordings\/mp3\/#{work.title.gsub(' ', '_')}\.pdf/)
+    expect(rendered).to match(/recordings\/mp3\/#{work.title.gsub(' ', '_')}\.mp3/)
+    work.destroy
   end
 end
