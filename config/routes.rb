48b43ab70cf25437dfa263580677ef50e92d7493
Michael Toulouse <wrycoder@ameritech.net>
2025-02-06 02:35:06 +0000

    Adding route for liner notes

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
                                  :index, :edit, :update]
   resources :genres, only: [:new, :create, :index, :update]
 
+  resources :liner_notes, only: [:new, :create, :edit, :update, :show]
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
