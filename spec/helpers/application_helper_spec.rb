e76bb23b063458c421083ace27ab5c2cefc12ff0
Michael Toulouse <wrycoder@ameritech.net>
2025-02-06 21:54:57 +0000

    Spec for application helper

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe ApplicationHelper, type: :helper do
+  before :each do
+   if !ENV['FILE_HOST'].nil?
+      @original_file_host = ENV['FILE_HOST']
+    else
+      ENV['FILE_HOST'] = 'sowash.com'
+      @original_file_host = nil
+    end
+
+    if !ENV['FILE_ROOT'].nil?
+      @original_file_root = ENV['FILE_ROOT']
+    else
+      ENV['FILE_ROOT'] = 'graphics/menu'
+      @original_file_root = nil
+    end
+  end
+
+  after :each do
+    ENV['FILE_HOST'] = @original_file_host
+    ENV['FILE_ROOT'] = @original_file_root
+  end
+
+  it "can create the composer menu", focus: true do
+    sheetmusic_prompt = composer_menu_prompts[:sheetmusic]
+    expect(sheetmusic_prompt[0]).to match(
+      /sowash\.com\/graphics\/menu\/menu2a\.gif/
+    )
+  end
+end
