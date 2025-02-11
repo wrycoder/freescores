c531eb0326f7766146aeaae1bdb0b1dc4f0d6c78
Michael Toulouse <wrycoder@ameritech.net>
2025-02-11 13:04:42 +0000

    A proper spaceship operator for instrument comparisons

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -2,4 +2,8 @@ class Instrument < ApplicationRecord
   has_many  :parts
   validates :name, presence: true, uniqueness: true
   validates :family, presence: true
+
+  def <=>(other)
+    rank <=> other.rank
+  end
 end
