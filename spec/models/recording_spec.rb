e97f5b25687a8727d687e191a24d934859d4a02e
Michael Toulouse <wrycoder@ameritech.net>
2025-02-13 21:38:58 +0000

    Test validators for recordings and scores

--- spec/models/recording_spec.rb
+++ spec/models/recording_spec.rb
@@ -8,8 +8,19 @@ RSpec.describe Recording, type: :model do
       r.work = nil
       expect(r.valid?).to be false
     end
-  end
 
-  context "at runtime" do
+    it "has the correct format" do
+      r = build(:recording, file_name: "foobar.foo")
+      expect(r.valid?).to be false
+      expect(r.errors.messages).to_not be nil
+      expect(r.errors.messages[:file_name][0])
+        .to match(/must be MP3/)
+    end
+
+    it "has a label" do
+      r = build(:recording, label: nil)
+      expect(r.valid?).to be false
+      expect(r.errors.messages[:label].empty?).to be false
+    end
   end
 end
--- spec/models/score_spec.rb
+++ spec/models/score_spec.rb
@@ -8,6 +8,20 @@ RSpec.describe Score, type: :model do
       s.work = nil
       expect(s.valid?).to be false
     end
+
+    it "has the correct format" do
+      s = build(:score, file_name: "basic.foo")
+      expect(s.valid?).to be false
+      expect(s.errors.messages).to_not be nil
+      expect(s.errors.messages[:file_name][0])
+        .to match(/must be in PDF/)
+    end
+
+    it "has a label" do
+      s = build(:score, label: nil)
+      expect(s.valid?).to be false
+      expect(s.errors.messages[:label].empty?).to be false
+    end
   end
 
   context "at runtime" do
