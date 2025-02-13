94bd3bf91368630199e1ea4ccc10b49c94989479
Michael Toulouse <wrycoder@ameritech.net>
2025-02-13 21:31:52 +0000

    Adding simple validations for recordings and scores

--- app/models/recording.rb
+++ app/models/recording.rb
@@ -1,3 +1,8 @@
 class Recording < ApplicationRecord
   belongs_to :work
+  validates_presence_of :label
+  validates :file_name,
+    presence: true,
+    format: { with: /\.mp3\z/,
+      message: 'must be MP3' }
 end
