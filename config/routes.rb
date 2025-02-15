c626a8090e8b479c81db2d7ff25dd92d1b756f94
Michael Toulouse <wrycoder@ameritech.net>
2025-02-15 22:30:44 +0000

    Adding routes for vocal and instrumental

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get 'sessions/destroy'
   get 'sessions/new'
   get 'works/search', to: 'works#search'
+  get 'works/vocal', to: 'works#vocal'
+  get 'works/instrumental', to: 'works#instrumental'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :instruments, only: [:new, :create, :show,
