7099434ad847257dfe1e7d0dc12f821272b47758
Michael Toulouse <deryankee@gmail.com>
2025-03-10 13:48:58 +0000

    Adding system utilities to local-bin

---
+++ local-bin/sowash-freescores.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export ADMIN_PASSWORD=uo*ZRPjVdz6
+export APP_HOST=https://sowash.blurve.net
+export MEDIA_HOST=https://sowash.com
+export FILE_ROOT=recordings/mp3
+export SECRET_KEY_BASE=14ad34c8e41b17f0af6699c7abf71b52
+export RAILS_ENV=production
+
+/home/michael/.rbenv/shims/bundle exec rails s -p 8181 -b 0.0.0.0
+
